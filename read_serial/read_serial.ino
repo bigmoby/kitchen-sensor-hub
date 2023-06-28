8321c6dc1015f21ff963fa6ae477d3a6ec479c78
Fabio Bigmoby Mauro <ing.fabiomauro@gmail.com>
2023-06-28 23:20:24 +0200

    fixing reader

--- read_serial/read_serial.ino
+++ read_serial/read_serial.ino
@@ -1,13 +1,40 @@
 #include <TimeLib.h>
 #include <TFT_eSPI.h>  // Include the graphics library (this includes the sprite functions)
-#include "NotoSansBold36.h"
+#include "Free_Fonts.h"
+#include <TJpg_Decoder.h>
+#include "number.h"
+#include "img/temperature.h"
+#include "img/humidity.h"
+#include "img/pangzi/i0.h"
+#include "img/pangzi/i1.h"
+#include "img/pangzi/i2.h"
+#include "img/pangzi/i3.h"
+#include "img/pangzi/i4.h"
+#include "img/pangzi/i5.h"
+#include "img/pangzi/i6.h"
+#include "img/pangzi/i7.h"
+#include "img/pangzi/i8.h"
+#include "img/pangzi/i9.h"
+#include "font/ZdyLwFont_20.h"
 
-TFT_eSPI tft = TFT_eSPI();  // Create object "tft"
+Number dig;
 
+TFT_eSPI tft = TFT_eSPI();               // Create object "tft"
 TFT_eSprite orario = TFT_eSprite(&tft);  // Create Sprite object "img" with pointer to "tft" object
                                          // the pointer is used by pushSprite() to push it onto the TFT
+TFT_eSprite data = TFT_eSprite(&tft);
 
-#define BITS_PER_PIXEL 16  // How many bits per pixel in Sprite
+TFT_eSprite umidita = TFT_eSprite(&tft);
+TFT_eSprite temperatura = TFT_eSprite(&tft);
+TFT_eSprite pressione = TFT_eSprite(&tft);
+
+TFT_eSprite message_banner = TFT_eSprite(&tft);
+
+#define BITS_PER_PIXEL 8  // How many bits per pixel in Sprite
+#define TFT_BETTER_ORANGE 0xFB80
+#define TFT_DARK_YELLOW 0xFC00
+#define IWIDTH 240
+#define IHEIGHT 30
 
 boolean newData = true;
 
@@ -15,9 +42,13 @@ boolean newData = true;
 char message[50];
 int alarm;
 long timestamp;
-float humidity;
+int humidity;
 float temperature;
 float pressure;
+
+int hours = 0;
+int minutes = 0;
+int seconds = 0;
 
 // Dimensione del buffer di input seriale
 const int bufferSize = 100;
@@ -26,42 +57,16 @@ const int bufferSize = 100;
 char buffer[bufferSize];
 int bufferIndex = 0;
 
-// Funzione per il parsing della stringa
-void parseString(const char* input) {
-  // Ignora il primo carattere "<"
-  input++;
+static int backgroundColor = TFT_BLACK;
 
-  // Copia la stringa di input nel buffer
-  strcpy(buffer, input);
-  bufferIndex = 0;
-
-  // Effettua il parsing dei valori
-  char* token = strtok(buffer, ",");
-  if (token != NULL) {
-    strcpy(message, token);
-    token = strtok(NULL, ",");
-  }
-  if (token != NULL) {
-    alarm = atoi(token);
-    token = strtok(NULL, ",");
-  }
-  if (token != NULL) {
-    timestamp = atol(token);
-    token = strtok(NULL, ",");
-  }
-  if (token != NULL) {
-    humidity = atof(token);
-    token = strtok(NULL, ",");
-  }
-  if (token != NULL) {
-    temperature = atof(token);
-    token = strtok(NULL, ",");
-  }
-  if (token != NULL) {
-    pressure = atof(token);
-    token = strtok(NULL, ",");
-  }
+bool tft_output(int16_t x, int16_t y, uint16_t w, uint16_t h, uint16_t* bitmap) {
+  if (y >= tft.height()) return 0;
+  tft.pushImage(x, y, w, h, bitmap);
+  // Return 1 to decode next block
+  return 1;
 }
+
+void digitalClockDisplay();
 
 // =========================================================================
 void setup(void) {
@@ -73,18 +78,96 @@ void setup(void) {
   Serial.println();
 
   tft.init();
-  tft.setRotation(2);
-  tft.fillScreen(TFT_NAVY);
+  tft.setRotation(1);
+  tft.invertDisplay(true);
+  tft.fillScreen(backgroundColor);
+
+  // Create the date sprite
+  data.setColorDepth(BITS_PER_PIXEL);  // Set colour depth first
+  data.createSprite(145, 15);          // then create the sprite
 
   // Create the clock sprite
   orario.setColorDepth(BITS_PER_PIXEL);  // Set colour depth first
-  orario.createSprite(90, 40);           // then create the sprite
+  orario.createSprite(122, 40);          // then create the sprite
 
-  // Only 1 font used in the sprite, so can remain loaded
-  orario.loadFont(NotoSansBold36);
+  // Create the message_banner sprite
+  message_banner.setColorDepth(BITS_PER_PIXEL);  // Set colour depth first
+  message_banner.createSprite(IWIDTH, IHEIGHT);  // then create the sprite
+
+  // Create the umidità sprite
+  umidita.setColorDepth(BITS_PER_PIXEL);  // Set colour depth first
+  umidita.createSprite(60, 20);           // then create the sprite
+
+  // Create the temperatura sprite
+  temperatura.setColorDepth(BITS_PER_PIXEL);  // Set colour depth first
+  temperatura.createSprite(80, 20);           // then create the sprite
+
+  // Create the pressione sprite
+  pressione.setColorDepth(BITS_PER_PIXEL);  // Set colour depth first
+  pressione.createSprite(120, 20);          // then create the sprite
+
+  TJpgDec.setJpgScale(1);
+  TJpgDec.setSwapBytes(true);
+  TJpgDec.setCallback(tft_output);
+  TJpgDec.drawJpg(10, 155, humidity_img, sizeof(humidity_img));
+  TJpgDec.drawJpg(35, 177, temperature_img, sizeof(temperature_img));
+
+  digitalClockDisplay();
 }
-// =========================================================================
+
+// ====================== Animation ==================================
+int Anim = 0;
+int AprevTime = 0;
+void imgAnim() {
+  int x = 160, y = 160;
+  if (millis() - AprevTime > 37) {
+    Anim++;
+    AprevTime = millis();
+  }
+  if (Anim == 10)
+    Anim = 0;
+
+  switch (Anim) {
+    case 0:
+      TJpgDec.drawJpg(x, y, i0, sizeof(i0));
+      break;
+    case 1:
+      TJpgDec.drawJpg(x, y, i1, sizeof(i1));
+      break;
+    case 2:
+      TJpgDec.drawJpg(x, y, i2, sizeof(i2));
+      break;
+    case 3:
+      TJpgDec.drawJpg(x, y, i3, sizeof(i3));
+      break;
+    case 4:
+      TJpgDec.drawJpg(x, y, i4, sizeof(i4));
+      break;
+    case 5:
+      TJpgDec.drawJpg(x, y, i5, sizeof(i5));
+      break;
+    case 6:
+      TJpgDec.drawJpg(x, y, i6, sizeof(i6));
+      break;
+    case 7:
+      TJpgDec.drawJpg(x, y, i7, sizeof(i7));
+      break;
+    case 8:
+      TJpgDec.drawJpg(x, y, i8, sizeof(i8));
+      break;
+    case 9:
+      TJpgDec.drawJpg(x, y, i9, sizeof(i9));
+      break;
+    default:
+      Serial.println("Error anim");
+      break;
+  }
+}
+
+// ==========================
 void loop() {
+  static bool alarmActive = false;
+  //imgAnim();
 
   if (Serial.available()) {
     // Leggi la stringa dalla seriale
@@ -100,12 +183,23 @@ void loop() {
       // Stampa i valori ottenuti dal parsing
       Serial.print("message: ");
       Serial.println(message);
+      drawBanner(&message_banner, message, 50, 130, TFT_RED);
+
       Serial.print("alarm: ");
       Serial.println(alarm);
+
+      if (alarm == 1) {
+        alarmActive = true;
+        backgroundColor = (backgroundColor == TFT_BLACK) ? TFT_RED : TFT_BLACK;
+      } else if (alarm == 0) {
+        alarmActive = false;
+        backgroundColor = TFT_BLACK;
+      }
 
       // Converte il timestamp in una data leggibile
       tmElements_t time;
       breakTime(timestamp, time);
+
       char timestampString[20];
       sprintf(timestampString, "%04d-%02d-%02d %02d:%02d:%02d",
               time.Year + 1970, time.Month, time.Day,
@@ -119,6 +213,10 @@ void loop() {
       char* hourToken = strtok(timeToken, ":");
       char* minuteToken = strtok(NULL, ":");
       char* secondToken = strtok(NULL, ":");
+
+      hours = atoi(hourToken);
+      minutes = atoi(minuteToken);
+      seconds = atoi(secondToken);
 
       // Array contenente i nomi dei mesi
       const char* monthNames[] = { "Gen", "Feb", "Mar", "Apr", "Mag", "Giu", "Lug", "Ago", "Set", "Ott", "Nov", "Dic" };
@@ -161,9 +259,15 @@ void loop() {
       Serial.print("pressure: ");
       Serial.println(pressure);
 
-      drawBanner(&orario, simplifiedTime, 40, 50, TFT_ORANGE);
+      drawBanner(&data, simplifiedDayOfWeek + String(" ") + String(day) + String(" ") + simplifiedMonth, 60, 30, TFT_LIGHTGREY);
+      //drawBanner(&orario, simplifiedTime, 60, 52, TFT_BETTER_ORANGE);
+      digitalClockDisplay();
 
-      delay(2000);
+      drawBanner(&umidita, String(humidity) + String("%"), 35, 158, TFT_LIGHTGREY);
+      drawBanner(&temperatura, String(temperature) + "℃", 59, 180, TFT_LIGHTGREY);
+      drawBanner(&pressione, String(pressure) + String(" hPa"), 65, 202, TFT_LIGHTGREY);
+
+      delay(3000);
     } else {
       // Aggiungi il carattere al buffer
       buffer[bufferIndex] = c;
@@ -171,29 +275,29 @@ void loop() {
     }
   }
 }
+
 // =========================================================================
 // Create sprite, plot graphics in it, plot to screen, then delete sprite
 // =========================================================================
 void drawBanner(TFT_eSprite* sprite, String text, int x, int y, int color) {
+  if (text) {
+    sprite->setColorDepth(8);
+    sprite->loadFont(ZdyLwFont_20);
+    sprite->fillSprite(backgroundColor);
+    sprite->setTextWrap(false);
+    sprite->setTextColor(color, backgroundColor);
+    sprite->setTextDatum(TL_DATUM);
 
-  // Create an 1 bit (2 colour) sprite 70x80 pixels (uses 70*80/8 = 700 bytes of RAM)
-  // Colour depths of 8 bits per pixel and 16 bits are also supported.
+    sprite->drawString(text, 0, 0, GFXFF);
+    // Push sprite to TFT screen CGRAM at coordinate x,y (top left corner)
+    // Specify what colour is to be treated as transparent (black in this example)
+    sprite->pushSprite(x, y, TFT_TRANSPARENT);
+    //  sprite->deleteSprite();
 
-  sprite->fillSprite(TFT_NAVY);
-  //sprite->fillSprite(TFT_TRANSPARENT);
-  sprite->setTextColor(TFT_ORANGE);
-  sprite->setTextDatum(TL_DATUM);
-  // Only 1 font used in the sprite, so can remain loaded
-  sprite->loadFont(NotoSansBold36);
-
-  sprite->drawString(text, 0, 0, 4);
-  // Push sprite to TFT screen CGRAM at coordinate x,y (top left corner)
-  // Specify what colour is to be treated as transparent (black in this example)
-  sprite->pushSprite(x, y, TFT_TRANSPARENT);
-  //  sprite->deleteSprite();
+    sprite->unloadFont();
+  }
 }
 // =========================================================================
-
 
 // Funzione per calcolare il giorno della settimana
 int calculateDayOfWeek(int year, int month, int day) {
@@ -212,4 +316,69 @@ int calculateDayOfWeek(int year, int month, int day) {
   }
 
   return dayOfWeek;
+}
+
+// Funzione per il parsing della stringa
+void parseString(const char* input) {
+
+  // Copia la stringa di input nel buffer droppando il primo carattere
+  strcpy(buffer, &input[2]);
+  bufferIndex = 0;
+
+  // Effettua il parsing dei valori
+  char* token = strtok(buffer, ",");
+  if (token != NULL) {
+    strcpy(message, token);
+    token = strtok(NULL, ",");
+  }
+  if (token != NULL) {
+    alarm = atoi(token);
+    token = strtok(NULL, ",");
+  }
+  if (token != NULL) {
+    timestamp = atol(token);
+    token = strtok(NULL, ",");
+  }
+  if (token != NULL) {
+    humidity = atoi(token);
+    token = strtok(NULL, ",");
+  }
+  if (token != NULL) {
+    temperature = round1(atof(token));
+    token = strtok(NULL, ",");
+  }
+  if (token != NULL) {
+    pressure = atof(token);
+    token = strtok(NULL, ",");
+  }
+}
+
+float round1(float value) {
+  long val = (long)(value * 10L);
+  value = (float)val / 10.0;
+  return value;
+}
+
+// ================= PRINT CLOCK ===========================
+unsigned char Hour_sign = 60;
+unsigned char Minute_sign = 60;
+unsigned char Second_sign = 60;
+uint16_t bgColor = 0x0000;
+void digitalClockDisplay() {
+  int timey = 52;
+  if (hours != Hour_sign) {
+    dig.printfW3660(20, timey, hours / 10);
+    dig.printfW3660(60, timey, hours % 10);
+    Hour_sign = hours;
+  }
+  if (minutes != Minute_sign) {
+    dig.printfO3660(101, timey, minutes / 10);
+    dig.printfO3660(141, timey, minutes % 10);
+    Minute_sign = minutes;
+  }
+  if (seconds != Second_sign) {
+    dig.printfW1830(182, timey + 30, seconds / 10);
+    dig.printfW1830(202, timey + 30, seconds % 10);
+    Second_sign = seconds;
+  }
 }
